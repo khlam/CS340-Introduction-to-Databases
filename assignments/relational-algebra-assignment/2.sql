82cb5356fcc5f13fa569ad197f7c595242830785
khlam <khlam@users.noreply.github.com>
2019-06-26 22:13:12 -0700

    final update

--- assignments/relational-algebra-assignment/2.sql
+++ assignments/relational-algebra-assignment/2.sql
@@ -1,4 +1,4 @@
-SELECT Make.make_name, Model.model_name FROM Vehicle
+SELECT Inventory.inventory_id, Make.make_name, Model.model_name FROM Vehicle
 	INNER JOIN Model
 	ON Model.model_id = Vehicle.fk_model_id
 	INNER JOIN Make
@@ -9,4 +9,4 @@ SELECT Make.make_name, Model.model_name FROM Vehicle
 	ON Color.color_id = Inventory.fk_color_id
 WHERE Color.name = 'Sky Blue';
 
-#π Make.make_name, Model.model_name (σ Color.name = 'Sky Blue' ((((Vehicle ⨝ Model.model_id = Vehicle.fk_model_id Model )⨝ Make.make_id = Vehicle.fk_make_id Make)⨝ Inventory.fk_vehicle_id = Vehicle.vehicle_id Inventory)⨝ Inventory.fk_color_id = Color.color_id Color))
+#π Inventory.inventory_id, Make.make_name, Model.model_name (σ Color.name = 'Sky Blue' ((((Vehicle ⨝ Model.model_id = Vehicle.fk_model_id Model )⨝ Make.make_id = Vehicle.fk_make_id Make)⨝ Inventory.fk_vehicle_id = Vehicle.vehicle_id Inventory)⨝ Inventory.fk_color_id = Color.color_id Color))
--- assignments/relational-algebra-assignment/3.sql
+++ assignments/relational-algebra-assignment/3.sql
@@ -1,4 +1,4 @@
-SELECT Make.make_name, Model.model_name, Incentive.amount FROM Vehicle
+SELECT Inventory.inventory_id, Make.make_name, Model.model_name, Incentive.amount FROM Vehicle
 	INNER JOIN Vehicle_Incentive
 	ON Vehicle_Incentive.fk_vehicle_id = Vehicle.vehicle_id
 	INNER JOIN Incentive
@@ -7,6 +7,8 @@ SELECT Make.make_name, Model.model_name, Incentive.amount FROM Vehicle
 	ON Model.model_id = Vehicle.fk_model_id
 	INNER JOIN Make
 	ON Make.make_id = Vehicle.fk_make_id
+	INNER JOIN Inventory
+	ON Inventory.fk_vehicle_id = Vehicle.vehicle_id
 WHERE Incentive.type = 'dealer';
 
-# π Make.make_name, Model.model_name, Incentive.amount (σ Incentive.type = 'dealer' (((Vehicle ⨝ Vehicle_Incentive.fk_vehicle_id = Vehicle.vehicle_id Vehicle_Incentive) ⨝ Vehicle_Incentive.fk_incentive_id = Incentive.incentive_id Incentive) ⨝ Model.model_id = Vehicle.fk_model_id Model) ⨝ Make.make_id = Vehicle.fk_make_id Make)
+# π Inventory.inventory_id, Make.make_name, Model.model_name, Incentive.amount (σ Incentive.type = 'dealer' ((((Vehicle ⨝ Vehicle_Incentive.fk_vehicle_id = Vehicle.vehicle_id Vehicle_Incentive) ⨝ Vehicle_Incentive.fk_incentive_id = Incentive.incentive_id Incentive) ⨝ Model.model_id = Vehicle.fk_model_id Model) ⨝ Make.make_id = Vehicle.fk_make_id Make) ⨝ Inventory.fk_vehicle_id = Vehicle.vehicle_id Inventory)
